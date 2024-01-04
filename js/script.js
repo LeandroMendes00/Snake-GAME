84d258b44cbe989d3a9bf58ed8369a4c1eb9fccc
LeandroMendes00 <leandr12eu@gmail.com>
2024-01-04 15:40:02 -0300

    criando o gameLoop

--- js/script.js
+++ js/script.js
@@ -3,10 +3,7 @@ const ctx = canvas.getContext("2d")
 
 const size = 30
 
-const snake = [
-    { x: 200, y:200},
-    { x: 230, y:200},   
-]
+const snake = [{ x: 0, y:0}]
 
 let direction, loopId
 
@@ -46,10 +43,30 @@ const moveSnake = () => {
     snake.shift()
 }
 
+const drawGrid = () => {
+    ctx.lineWidth = 1   
+    ctx.strokeStyle = "white"
+    
+    for (let i = 30; i < canvas.width; i += 30) {
+        ctx.beginPath()
+        ctx.lineTo(i, 0)
+        ctx.lineTo(i, 600)
+        ctx.stroke()
+
+        ctx.beginPath()
+        ctx.lineTo(0, i)
+        ctx.lineTo(600, i)
+        ctx.stroke()
+    }    
+}
+
+drawGrid()
+
 const gameLoop = () => {
     clearInterval(loopId)
     
     ctx.clearRect(0, 0, 600, 600)
+    drawGrid()
     moveSnake()
     drawSnake()
     
@@ -60,5 +77,22 @@ const gameLoop = () => {
 
 gameLoop()
 
+document.addEventListener("keydown", ({ key }) => {
+    if (key == "ArrowRight" && direction != "left" ) {
+        direction = "right"
+    }
+
+    if (key == "ArrowLeft" && direction != "right") {
+        direction = "left"
+    }
+
+    if (key == "ArrowDown" && direction != "up") {
+        direction = "down"
+    }
+
+    if (key == "ArrowUp" && direction != "down") {
+        direction = "up"
+    }
+})
 
 
